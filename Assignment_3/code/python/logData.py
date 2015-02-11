5297b525cb56f2b1673aadb97f0e72bc7cce9fe8
Alexander Huynh <AHuynh@users.noreply.github.com>
2015-02-10 20:03:11 -0500

    Add correctly-implemented logData

--- Assignment_3/code/python/logData.py
+++ Assignment_3/code/python/logData.py
@@ -6,10 +6,17 @@ from Scribbler2 import *
 
 fname = "log-%d.txt" % time.time()
 
-s = Scribbler2('/dev/rfcomm0',fname)
+s = Scribbler2('COM8', fname)
+
+# command constants (time)
+T_WAT = .6
+T_TRN = 1.2
+# command constants (power)
+P_FWD = 200
+P_TRN = 100
 
 # Set timeout to zero
-print 'Connected!'
+print ('Connected!')
 s.setIRPower(140)
 s.setForwardness(1)
 # Create a list of commands
@@ -17,8 +24,20 @@ commands = []
 # Command is a list [cmd, leftMotor, rightMotor, time]
 # Setting motors to 200 will drive 
 # forward with the fluke facing forward
-commands.append([200, 200, 4])
-commands.append([0, 0, 2])
+commands.append([P_FWD, P_FWD, 2.1])     # FWD
+commands.append([0, 0, T_WAT])           # WAIT
+commands.append([-P_TRN, P_TRN, T_TRN])  # TURN LEFT
+commands.append([0, 0, T_WAT])           # WAIT
+commands.append([P_FWD, P_FWD, 1.9])     # FWD
+commands.append([0, 0, T_WAT])           # WAIT
+commands.append([P_TRN, -P_TRN, T_TRN])  # TURN RIGHT
+commands.append([0, 0, T_WAT])           # WAIT
+commands.append([P_FWD, P_FWD, 4.4])     # FWD
+commands.append([0, 0, T_WAT])           # WAIT
+commands.append([P_TRN, -P_TRN, T_TRN])  # TURN RIGHT
+commands.append([0, 0, T_WAT])           # WAIT
+commands.append([P_FWD, P_FWD, 3.0])     # FWD
+commands.append([0, 0, T_WAT])           # WAIT
 print ("Start!")
 for c in commands:
   start = time.time()
@@ -26,6 +45,6 @@ for c in commands:
   while (time.time() - start < c[2]):
     s.getObstacle(1)
     time.sleep(0.1) # Read sensors at 1Hz
-
+s.log.close()
 
 
